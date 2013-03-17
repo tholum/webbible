96c41c21a820947310339b8184800144bbebc58e
tim.holum <webbible@couleetechlink.com>
2013-03-17 13:50:52 -0500

    Making Tabs more dynamic

--- bible/index.php
+++ bible/index.php
@@ -82,7 +82,7 @@ slimcrm.bible = ['Genesis','Exodus','Leviticus','Numbers','Deuteronomy','Joshua'
 <% _(books).each(function( book ){ %><option value="<%= book %>" ><%= book %></option><% } ) %>
 </script>
 <script type="text/template" id="tab-inner">
-<div id="tab_<%= id %>" class="tab" >
+<div id="<%= id %>" class="tab" >
 			<div>
 					<select class="book" >
 						<option value="ESV" >ESV</option>
