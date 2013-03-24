5c322d6bdea5a178f3f0033e131c7004da7e6cc1
Tim Holum <tholum@couleetechlink.com>
2013-03-24 11:10:53 -0500

    adding modules.cpp

---
+++ cgi/modules.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <rawtext.h>
+#include <swmgr.h>
+#include <versekey.h>
+#include <markupfiltmgr.h>
+#include <regex.h> // GNU
+#include <iostream>
+
+#ifndef NO_SWORD_NAMESPACE
+using namespace sword;
+#endif
+
